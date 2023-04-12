e6aa576d10eee6507c1843b1b75d53431787c3e6
john <john@jianshukeji.com>
2023-04-12 13:49:41 +0800

    fixed file name

--- app/(code)/layout.js
+++ app/(code)/layout.js
@@ -1,8 +1,8 @@
 import '@styles/globals.scss';
 import "@styles/code/index.scss";
 
-import Header from '@components/header';
-import Footer from '@components/footer';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
 
 export default function RootLayout({ children }) {
 	return (
